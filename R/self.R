b25a14de72e44a57228e4393135d051c1a7a34ec
yangquan0310 <yangquan0310@163.com>
2023-12-08 18:23:31 +0800

    first

--- R/self.R
+++ R/self.R
@@ -1,6 +1,34 @@
 library(R6)
 self<-R6Class(
   classname = "self",
-  public = list(),
+  public = list(
+    trait=c(),
+    structure=matrix(),
+    initialize=function(trait,structure){
+      self$trait=trait
+      self$structure=structure
+      dimnames(self$structure)=list(
+        self$trait,
+        self$trait
+      )
+      
+    },
+    get_self_structure=function(){
+      return(self$structure)
+    }
+  ),
   private = list()
 )
+if (interactive()) {
+  test=self$new(
+    trait=c("happy","sad"),
+    structure = matrix(
+      c(
+        1,0,
+        0,1
+      ),
+      nrow=2,ncol=2
+    )
+  )
+  test$get_self_structure()
+}
